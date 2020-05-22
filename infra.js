19776cd0b7edf829e7a62625908addde66911810
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-05-22 09:48:36 +0400

    fix

--- infra.js
+++ infra.js
@@ -1,10 +1,11 @@
 import { CDN } from '/vendor/akiyatkin/load/CDN.js'
 import { CDEK } from '/vendor/akiyatkin/cdek/CDEK.js'
 import { Event } from '/vendor/infrajs/event/Event.js'
+import { DOM } from '/vendor/akiyatkin/load/DOM.js'
 
 let cls = cls => document.getElementsByClassName(cls)
 let ws = new WeakSet()
-Event.handler('Controller.onshow', async () => {
+DOM.done('load', async () => {
 	let btns = cls('-cdek-city')
 	await CDN.on('load','jquery')
 	let city = Session.get('orders.my.cdek.wat.cityName', Config.get('cdek').defaultCity)
